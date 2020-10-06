5d9517b151bf382a5f7926f8f0d7c80dbabda926
Timothee Mickus <timothee.mickus@univ-lorraine.fr>
2020-10-06 16:23:40 +0200

    comm

--- src/exp3_embs/get_randtf_embs.py
+++ src/exp3_embs/get_randtf_embs.py
@@ -13,21 +13,23 @@ def get_random_embedding():
 if __name__ == "__main__":
 	import argparse
 	p = argparse.ArgumentParser("Make random sentence embeddings with an untrained Transformer")
-	
+
 	p.add_argument("--input_dir", type=str, help="input directory", required=True)
 	p.add_argument("--pickle", type=str, help="pickle filepath to save model", default="randlstm.pkl")
 
 	args = p.parse_args()
 	print(datetime.datetime.now(), "process start")
-	
+
 	# model
 	encoder_layer = nn.TransformerEncoderLayer(d_model=512, nhead=8)
 	transformer = nn.TransformerEncoder(encoder_layer, num_layers=6)
 	for name, param in transformer.named_parameters():
+		# a 0-constant init would entail gain parameters at 0, so 0-valued vectors.
+		# it would be neater to initialize biases with 0 and gains with 1.
 		nn.init.uniform_(param, -0.1, 0.1)
 		if param.dim() > 1:
 			nn.init.xavier_uniform_(param)
-		
+
 
 	pe = torch.zeros(512, 512)
 	position = torch.arange(0, 512, dtype=torch.float).unsqueeze(1)
@@ -39,14 +41,14 @@ if __name__ == "__main__":
 	# data
 	input_files = (
 		os.path.join(root, filename)
-		for root, _, filenames in os.walk(args.input_dir) 
-		for filename in filenames 
+		for root, _, filenames in os.walk(args.input_dir)
+		for filename in filenames
 		if filename.endswith(".txt")
 	)
 	vocab = collections.defaultdict(get_random_embedding)
 
 	torch.set_grad_enabled(False)
-	for input_file in input_files:	
+	for input_file in input_files:
 		print(datetime.datetime.now(), "handling file %s" % input_file)
 		with open(input_file) as istr, open(input_file+ ".rd.tsv", "w") as ostr:
 			writer = csv.writer(ostr, delimiter="\t")
@@ -59,4 +61,3 @@ if __name__ == "__main__":
 	print(datetime.datetime.now(), "saving Transformer")
 	with open(args.pickle, "wb") as ostr:
 		pickle.dump((vocab, transformer), ostr)
-	
