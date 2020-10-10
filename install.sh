7921068ffc09f5f82267a9995b1b6d6b58e44140
Timothee Mickus <timothee.mickus@univ-lorraine.fr>
2020-10-10 16:18:58 +0200

    update install shell to download word embeddings

--- install.sh
+++ install.sh
@@ -5,19 +5,17 @@ echo '0a. Setting up python3 environment'
 virtualenv --python=/usr/bin/python3.7 .venv3
 source .venv3/bin/activate
 pip3 install -r pip3.requirements.txt
-
 python3 -m spacy download en_core_web_sm
 python3 -c "import nltk; nltk.download('wordnet')"
 python3 -c "import nltk; nltk.download('punkt')"
-
 deactivate
 
 echo '0a. Setting up python2 environment'
 virtualenv --python=/usr/bin/python2 .venv2
 source .venv2/bin/activate
 pip2 install -r pip2.requirements.txt
-
 deactivate
+
 
 echo '1. Retrieving word embeddings'
 for ARCH in w2v ft gv6 gv840
@@ -25,14 +23,24 @@ do
   mkdir -p data/exp2/embs/${ARCH}
 done
 echo '1a. Retrieving word2vec (GoogleNews)'
+source .venv3/bin/activate
 gdown https://drive.google.com/u/0/uc?id=0B7XkCwpI5KDYNlNUTTlSS21pQmM;
 gunzip -d GoogleNews-vectors-negative300.bin.gz
-mv mv GoogleNews-vectors-negative300.bin data/exp2/embs/w2v/
+mv GoogleNews-vectors-negative300.bin data/exp2/embs/w2v/
+deactivate;
+
 echo '1b. Retrieving fastText (CommonCrawl)'
+wget -P data/exp2/embs/ft/ https://dl.fbaipublicfiles.com/fasttext/vectors-crawl/cc.en.300.bin.gz
+gunzip -d data/exp2/embs/ft/cc.en.300.bin.gz
+
 echo '1c. Retrieving GloVe (6B)'
+wget -P data/exp2/embs/gv6/ http://nlp.stanford.edu/data/glove.6B.zip
+unzip -d data/exp2/embs/gv6/ data/exp2/embs/gv840/glove.6B.zip
+
 echo '1d. Retrieving GloVe (840B)'
 wget -P data/exp2/embs/gv840/ http://nlp.stanford.edu/data/glove.840B.300d.zip;
 unzip -d data/exp2/embs/gv840/ data/exp2/embs/gv840/glove.840B.300d.zip
+
 
 echo '2. Download & set up external git repos'
 echo "2a. J. W. Carr's MantelTest"
@@ -58,10 +66,12 @@ ln -s data/exp2/embs/gv840 src/exp3_embs/InferSent/embs
 mkdir src/exp3_embs/InferSent/encoder
 wget -P src/exp3_embs/InferSent/encoder https://dl.fbaipublicfiles.com/infersent/infersent1.pkl
 
+
 echo '3. Evaluation data'
 echo '3a. Bruni & al. MEN dataset'
 wget -P data/ https://staff.fnwi.uva.nl/e.bruni/resources/MEN.zip
 unzip -d data data/MEN.zip
+
 echo '3b. Baroni & al. SICK dataset'
 wget -P data/ https://zenodo.org/record/2787612/files/SICK.zip
 unzip -d data/SICK data/SICK.zip
