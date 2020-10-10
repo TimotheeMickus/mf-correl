3cbf12abc077efd96eb1aed1601894c8fa43f75a
Timothee Mickus <timothee.mickus@univ-lorraine.fr>
2020-10-10 21:56:49 +0200

    readability

--- exp2.sh
+++ exp2.sh
@@ -1,105 +1,159 @@
 #!/usr/bin/env bash
 
-echo -e '\e[41m\e[1m 0. set up: activate virtual env, create required directories  \e[0m';
-
+echo -e '\e[33m\e[1m 0. set up: activate virtual env, create required directories  \e[0m';
 VENV3_ACTIVATION=.venv3/bin/activate;
+source $VENV3_ACTIVATION;
 
 EMB_W2V="data/exp2/embs/w2v/GoogleNews-vectors-negative300.bin";
 EMB_FT="data/exp2/embs/ft/cc.en.300.bin";
 EMB_GV6="data/exp2/embs/gv6/glove.6B.300d.txt";
 EMB_GV840="data/exp2/embs/gv840/glove.840B.300d.txt";
 
-source $VENV3_ACTIVATION;
 
 for SCENARIO in base paraphrase; do
   for EMB_ARCH in w2v ft gv6 gv840; do
     mkdir -p data/embs_exp2/${EMB_ARCH}/${SCENARIO};
   done;
-  mkdir -p data/trees_exp2/${SCENARIO};
+  mkdir -p data/trees_exp2/${SCENARIO} data/results_exp2/dists/${SCENARIO};
 done;
+mkdir -p data/results_exp2/mantels data/results_exp2/men;
 
-mkdir -p data/results_exp2/mantels data/results_exp2/men data/results_exp2/dists;
 
-
-echo -e '\e[41m\e[1m 1a. retrieve embeddings \e[0m';
+echo -e '\e[33m\e[1m 1a. retrieve embeddings \e[0m';
 for SCENARIO in base paraphrase; do
-  echo -e "\e[41m\e[1m ${SCENARIO} \e[0m";
-  echo -e '\e[41m\e[1m Word2Vec \e[0m';
+  echo -e "\e[33m\e[1m ${SCENARIO} \e[0m";
+  echo -e '\e[33m\e[1m Word2Vec \e[0m';
   for FILE in $(find data/exp2/csv/$SCENARIO -type f); do
     OUTPUT_FILE="data/embs_exp2/w2v/$SCENARIO/$(basename ${FILE%.csv}.tsv)";
-    python3 src/exp2_make_embeddings_file.py --input_file $FILE --output_file $OUTPUT_FILE --embs_path ${EMB_W2V} --is_binary;
+    python3 src/exp2_make_embeddings_file.py \
+      --input_file $FILE \
+      --output_file $OUTPUT_FILE \
+      --embs_path ${EMB_W2V} \
+      --is_binary;
   done;
-
-  echo -e '\e[41m\e[1m FastText \e[0m';
+  echo -e '\e[33m\e[1m FastText \e[0m';
   for FILE in $(find data/exp2/csv/$SCENARIO -type f); do
     OUTPUT_FILE="data/embs_exp2/ft/$SCENARIO/$(basename ${FILE%.csv}.tsv)";
-    python3 src/exp2_make_embeddings_file.py --input_file $FILE --output_file $OUTPUT_FILE --embs_path ${EMB_FT} --is_fasttext;
+    python3 src/exp2_make_embeddings_file.py \
+      --input_file $FILE \
+      --output_file $OUTPUT_FILE \
+      --embs_path ${EMB_FT} \
+      --is_fasttext;
   done;
-
-  echo -e '\e[41m\e[1m GloVe 6B \e[0m';
+  echo -e '\e[33m\e[1m GloVe 6B \e[0m';
   for FILE in $(find data/exp2/csv/$SCENARIO -type f); do
     OUTPUT_FILE="data/embs_exp2/gv6/$SCENARIO/$(basename ${FILE%.csv}.tsv)";
-    python3 src/exp2_make_embeddings_file.py --input_file $FILE --output_file $OUTPUT_FILE --embs_path ${EMB_GV6};
+    python3 src/exp2_make_embeddings_file.py \
+      --input_file $FILE \
+      --output_file $OUTPUT_FILE \
+      --embs_path ${EMB_GV6};
   done;
-
-  echo -e '\e[41m\e[1m GloVe 840B \e[0m';
+  echo -e '\e[33m\e[1m GloVe 840B \e[0m';
   for FILE in $(find data/exp2/csv/$SCENARIO -type f); do
     OUTPUT_FILE="data/embs_exp2/gv840/$SCENARIO/$(basename ${FILE%.csv}.tsv)";
-    python3 src/exp2_make_embeddings_file.py --input_file $FILE --output_file $OUTPUT_FILE --embs_path ${EMB_GV840};
+    python3 src/exp2_make_embeddings_file.py \
+      --input_file $FILE \
+      --output_file $OUTPUT_FILE \
+      --embs_path ${EMB_GV840};
   done;
 done;
 
-echo -e '\e[41m\e[1m 1c. evaluate on MEN \e[0m';
+echo -e '\e[33m\e[1m 1c. evaluate on MEN \e[0m';
 MEN_PATH="data/MEN/MEN_dataset_natural_form_full";
-echo -e '\e[41m\e[1m Word2Vec \e[0m';
-python3 src/exp2_test_men.py --embs ${EMB_W2V} --men_path ${MEN_PATH} --output_file "data/results_exp2/men/w2v-results-men.txt" --is_binary;
+echo -e '\e[33m\e[1m Word2Vec \e[0m';
+python3 src/exp2_test_men.py \
+  --embs ${EMB_W2V} \
+  --men_path ${MEN_PATH} \
+  --output_file "data/results_exp2/men/w2v-results-men.txt" \
+  --is_binary;
+echo -e '\e[33m\e[1m FastText \e[0m';
+python3 src/exp2_test_men.py \
+  --embs ${EMB_FT} \
+  --men_path ${MEN_PATH} \
+  --output_file "data/results_exp2/men/ft-results-men.txt" \
+  --is_fasttext;
+echo -e '\e[33m\e[1m GloVe 6B \e[0m';
+python3 src/exp2_test_men.py \
+  --embs ${EMB_GV6} \
+  --men_path ${MEN_PATH} \
+  --output_file "data/results_exp2/men/gv6-results-men.txt";
+echo -e '\e[33m\e[1m GloVe 840B \e[0m';
+python3 src/exp2_test_men.py \
+  --embs ${EMB_GV840} \
+  --men_path ${MEN_PATH} \
+  --output_file "data/results_exp2/men/gv840-results-men.txt";
 
-echo -e '\e[41m\e[1m FastText \e[0m';
-python3 src/exp2_test_men.py --embs ${EMB_FT} --men_path ${MEN_PATH} --output_file "data/results_exp2/men/ft-results-men.txt" --is_fasttext;
-
-echo -e '\e[41m\e[1m GloVe 6B \e[0m';
-python3 src/exp2_test_men.py --embs ${EMB_GV6} --men_path ${MEN_PATH} --output_file "data/results_exp2/men/gv6-results-men.txt";
-
-echo -e '\e[41m\e[1m GloVe 840B \e[0m';
-python3 src/exp2_test_men.py --embs ${EMB_GV840} --men_path ${MEN_PATH} --output_file "data/results_exp2/men/gv840-results-men.txt";
-
-
-echo -e '\e[41m\e[1m 2a. compute distances (meaning, Levenshtein & Jaccard) \e[0m';
+echo -e '\e[33m\e[1m 2a. compute distances (meaning, Levenshtein & Jaccard) \e[0m';
 ## computing form distances is only required once.
 EMB_ARCH='w2v';
-echo -e "\e[41m\e[1m ${EMB_ARCH} \e[0m";
+echo -e "\e[33m\e[1m ${EMB_ARCH} \e[0m";
 for EMB_FILE in $(find data/embs_exp2/${EMB_ARCH} -type f -name '*.tsv'); do
   OUTPUT_FILE="${EMB_FILE%.tsv}.json";
-  echo -e "\e[41m\e[1m ${EMB_FILE} to ${OUTPUT_FILE} \e[0m";
-  python3 src/shared/compute_distances.py --input $EMB_FILE --output ${OUTPUT_FILE};
+  echo -e "\e[33m\e[1m ${EMB_FILE} => ${OUTPUT_FILE} \e[0m";
+  python3 src/shared/compute_distances.py \
+    --input $EMB_FILE \
+    --output ${OUTPUT_FILE};
 done;
 
 for EMB_ARCH in ft gv6 gv840; do
-  echo -e "\e[41m\e[1m ${EMB_ARCH} \e[0m";
+  echo -e "\e[33m\e[1m ${EMB_ARCH} \e[0m";
   for EMB_FILE in $(find data/embs_exp2/${EMB_ARCH} -type f -name '*.tsv'); do
     OUTPUT_FILE="${EMB_FILE%.tsv}.json";;
-    echo -e "\e[41m\e[1m ${EMB_FILE} => ${OUTPUT_FILE} \e[0m";
-    python3 src/shared/compute_distances.py --input $EMB_FILE --output ${OUTPUT_FILE} --meaning_only;
+    echo -e "\e[33m\e[1m ${EMB_FILE} => ${OUTPUT_FILE} \e[0m";
+    python3 src/shared/compute_distances.py \
+      --input $EMB_FILE \
+      --output ${OUTPUT_FILE} \
+      --meaning_only;
   done
 done;
 
-echo -e '\e[41m\e[1m 2b. compute distances (TED) \e[0m';
+echo -e '\e[33m\e[1m 2b. compute distances (TED) \e[0m';
 for SCENARIO in base paraphrase; do
-  echo -e "\e[41m\e[1m ${SCENARIO} \e[0m";
+  echo -e "\e[33m\e[1m ${SCENARIO} \e[0m";
   for FILE in $(find data/exp2/csv/${SCENARIO} -type f); do
     OUTPUT_FILE="data/exp2_trees/${SCENARIO}/$(basename ${FILE%.csv}.trees.tsv)";
-    echo -e "\e[41m\e[1m ${FILE} => ${OUTPUT_FILE} \e[0m";
-    python3 src/exp2_compute_trees.py --input_file $FILE --output_file $OUTPUT_FILE;
+    echo -e "\e[33m\e[1m ${FILE} => ${OUTPUT_FILE} \e[0m";
+    python3 src/exp2_compute_trees.py \
+      --input_file $FILE \
+      --output_file $OUTPUT_FILE;
   done;
 done;
 for FILE in $(find data/exp2_trees -type f); do
-  OUTPUT_FILE="${FILE.tsv}.dists.tsv";
-  echo -e "\e[41m\e[1m ${FILE} => ${OUTPUT_FILE} \e[0m";
+  OUTPUT_FILE="${FILE%.tsv}.dists.tsv";
+  echo -e "\e[33m\e[1m ${FILE} => ${OUTPUT_FILE} \e[0m";
   java -jar src/shared/apted/bin/apted.jar $FILE > $OUTPUT_FILE;
 done;
 
-echo -e '\e[41m\e[1m 2c. patch, merge and sort everything. \e[0m';
+echo -e '\e[33m\e[1m 2c. patch, merge and sort everything. \e[0m';
+for EMB_ARCH in w2v ft gv6 gv840; do
+  python3 src/shared/merge.py \
+    --sort $(find data/embs_exp2/${EMB_ARCH} -type f -name '*.json') \
+    --key_meanings ${EMB_ARCH};
+done;
 
-echo -e '\e[41m\e[1m 3. compute Mantel tests \e[0m';
+for SCENARIO in base paraphrase; do
+  for RUN in $(seq 1 5); do
+    echo -e "\e[33m\e[1m merge all distances for run ${RUN}, scenario ${SCENARIO} \e[0m";
+    python3 src/shared/merge.py \
+      --merge $(find data/embs_exp2/ -type f -name "run-${RUN}.json" | grep $SCENARIO) \
+      --merged_file tmp-merge.json;
+    python3 src/shared/normalize_apted.py \
+      --output_file data/results_exp2/dists/${RUN}.json \
+      --tree_tsv_file data/trees_exp2/$SCENARIO/run-${RUN}.trees.dist.tsv \
+      --json_file tmp-merge.json;
+  done;
+done;
+
+
+echo -e '\e[33m\e[1m 3. compute Mantel tests \e[0m';
+for SCENARIO in base paraphrase; do
+  echo -e "\e[33m\e[1m ${SCENARIO} \e[0m";
+  python3 src/shared/compute_mantels_per_dir.py \
+    --input_dir data/results_exp2/dists/${SCENARIO}/ \
+    --output data/results_exp2/mantels/results-${SCENARIO}.txt;
+done;
+
+echo -e '\e[33m\e[1m 4. Perform annotation \e[0m';
+
 
 deactivate;
--- exp3.sh
+++ exp3.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-echo -e '\e[41m\e[1m 0. set up: activate virtual env, create required directories  \e[0m';
+echo -e '\e[33m\e[1m 0. set up: activate virtual env, create required directories  \e[0m';
 
 VENV3_ACTIVATION=.venv3/bin/activate;
 VENV2_ACTIVATION=.venv2/bin/activate;
@@ -13,80 +13,116 @@ done;
 mkdir -p data/results_exp3/mantels data/results_exp3/sick data/results_exp3/dists;
 
 
-echo -e '\e[41m\e[1m 1a. compute sentence embeddings \e[0m';
+echo -e '\e[33m\e[1m 1a. compute sentence embeddings \e[0m';
 #
-echo -e '\e[41m\e[1m USE \e[0m';
-python3 src/exp3_embs/get_use_embeddings.py --input_dir data/exp3 --output_dir data/embs_exp3/USE;
-echo -e '\e[41m\e[1m DAN \e[0m';
-python3 src/exp3_embs/get_use_dan_embeddings.py --input_dir data/exp3 --output_dir data/embs_exp3/DAN;
-echo -e '\e[41m\e[1m infersent \e[0m';
-python3 src/exp3_embs/get_infersent_embeddings.py --input_dir data/exp3 --output_dir data/embs_exp3/infersent;
+echo -e '\e[33m\e[1m USE \e[0m';
+python3 src/exp3_embs/get_use_embeddings.py \
+  --input_dir data/exp3 \
+  --output_dir data/embs_exp3/USE;
+echo -e '\e[33m\e[1m DAN \e[0m';
+python3 src/exp3_embs/get_use_dan_embeddings.py \
+  --input_dir data/exp3 \
+  --output_dir data/embs_exp3/DAN;
+echo -e '\e[33m\e[1m infersent \e[0m';
+python3 src/exp3_embs/get_infersent_embeddings.py \
+  --input_dir data/exp3 \
+  --output_dir data/embs_exp3/infersent;
 deactivate;
 
 source $VENV2_ACTIVATION;
-echo -e '\e[41m\e[1m skipthoughts \e[0m';
-python2 src/exp3_embs/get_skipthoughts_embeddings.py --input_dir data/exp3 --output_dir data/embs_exp3/skipthoughts;
+echo -e '\e[33m\e[1m skipthoughts \e[0m';
+python2 src/exp3_embs/get_skipthoughts_embeddings.py \
+  --input_dir data/exp3 \
+  --output_dir data/embs_exp3/skipthoughts;
 deactivate;
 
 source $VENV3_ACTIVATION;
-echo -e '\e[41m\e[1m 1b. compute random embeddings baselines \e[0m';
-echo -e '\e[41m\e[1m randlstm \e[0m';
-python3 src/exp3_embs/get_randlstm_embeddings.py --input_dir  data/exp3 --output_dir data/embs_exp3/randlstm --pickle data/embs_exp3/randlstm.pkl;
-echo -e '\e[41m\e[1m randtf \e[0m';
-python3 src/exp3_embs/get_randtf_embeddings.py --input_dir  data/exp3 --output_dir data/embs_exp3/randtf --pickle data/embs_exp3/randtf.pkl;
+echo -e '\e[33m\e[1m 1b. compute random embeddings baselines \e[0m';
+echo -e '\e[33m\e[1m randlstm \e[0m';
+python3 src/exp3_embs/get_randlstm_embeddings.py \
+  --input_dir data/exp3 \
+  --output_dir data/embs_exp3/randlstm \
+  --pickle data/embs_exp3/randlstm.pkl;
+echo -e '\e[33m\e[1m randtf \e[0m';
+python3 src/exp3_embs/get_randtf_embeddings.py \
+  --input_dir data/exp3 \
+  --output_dir data/embs_exp3/randtf \
+  --pickle data/embs_exp3/randtf.pkl;
 
-echo -e '\e[41m\e[1m 1c. evaluate on SICK \e[0m';
+echo -e '\e[33m\e[1m 1c. evaluate on SICK \e[0m';
 SICK_PATH='data/SICK/SICK.txt';
 for EMB_ARCH in USE DAN infersent; do
-  echo -e "\e[41m\e[1m ${EMB_ARCH} \e[0m";
-  python3 src/exp3_test_sick.py --emb_arch ${EMB_ARCH} --sick_path ${SICK_PATH} --output_file "data/results_exp3/sick/${EMB_ARCH}-results-sick.txt";
+  echo -e "\e[33m\e[1m ${EMB_ARCH} \e[0m";
+  python3 src/exp3_test_sick.py \
+    --emb_arch ${EMB_ARCH} \
+    --sick_path ${SICK_PATH} \
+    --output_file "data/results_exp3/sick/${EMB_ARCH}-results-sick.txt";
 done;
 deactivate;
 
 source $VENV2_ACTIVATION;
 EMB_ARCH=skipthoughts;
-echo -e "\e[41m\e[1m ${EMB_ARCH} \e[0m";
-python2 src/exp3_test_sick.py --emb_arch ${EMB_ARCH} --sick_path ${SICK_PATH} --output_file "data/results_exp3/sick/${EMB_ARCH}-results-sick.txt";
+echo -e "\e[33m\e[1m ${EMB_ARCH} \e[0m";
+python2 src/exp3_test_sick.py \
+  --emb_arch ${EMB_ARCH} \
+  --sick_path ${SICK_PATH} \
+  --output_file "data/results_exp3/sick/${EMB_ARCH}-results-sick.txt";
 deactivate;
 
 source $VENV3_ACTIVATION;
 for EMB_ARCH in randlstm randtf; do
-  echo -e "\e[41m\e[1m ${EMB_ARCH} \e[0m";
-  python3 src/exp3_test_sick.py --emb_arch ${EMB_ARCH} --emb_path data/embs_exp3/${EMB_ARCH}.pkl --sick_path ${SICK_PATH} --output_file "data/results_exp3/sick/${EMB_ARCH}-results-sick.txt";
+  echo -e "\e[33m\e[1m ${EMB_ARCH} \e[0m";
+  python3 src/exp3_test_sick.py \
+    --emb_arch ${EMB_ARCH} \
+    --emb_path data/embs_exp3/${EMB_ARCH}.pkl \
+    --sick_path ${SICK_PATH} \
+    --output_file "data/results_exp3/sick/${EMB_ARCH}-results-sick.txt";
 done;
 
-echo -e '\e[41m\e[1m 2a. compute distances \e[0m';
+echo -e '\e[33m\e[1m 2a. compute distances \e[0m';
 ## computing form distances is only required once.
 EMB_ARCH='USE';
-echo -e "\e[41m\e[1m ${EMB_ARCH} \e[0m";
+echo -e "\e[33m\e[1m ${EMB_ARCH} \e[0m";
 for EMB_FILE in $(find data/embs_exp3/${EMB_ARCH} -type f -name '*.emb.tsv'); do
   OUTPUT_FILE="${EMB_FILE%.emb.tsv}.json";
-  echo -e "\e[41m\e[1m ${EMB_FILE} to ${OUTPUT_FILE} \e[0m";
-  python3 src/shared/compute_distances.py --input $EMB_FILE --output ${OUTPUT_FILE} --no_jaccard;
+  echo -e "\e[33m\e[1m ${EMB_FILE} => ${OUTPUT_FILE} \e[0m";
+  python3 src/shared/compute_distances.py \
+    --input $EMB_FILE \
+    --output ${OUTPUT_FILE} \
+    --no_jaccard;
 done;
 
 for EMB_ARCH in DAN infersent skipthoughts randlstm randtf; do
-  echo -e "\e[41m\e[1m ${EMB_ARCH} \e[0m";
+  echo -e "\e[33m\e[1m ${EMB_ARCH} \e[0m";
   for EMB_FILE in $(find data/embs_exp3/${EMB_ARCH} -type f -name '*.emb.tsv'); do
     OUTPUT_FILE="${EMB_FILE%.emb.tsv}.json";
-    echo -e "\e[41m\e[1m ${EMB_FILE} => ${OUTPUT_FILE} \e[0m";
-    python3 src/shared/compute_distances.py --input $EMB_FILE --output ${OUTPUT_FILE} --meaning_only;
+    echo -e "\e[33m\e[1m ${EMB_FILE} => ${OUTPUT_FILE} \e[0m";
+    python3 src/shared/compute_distances.py \
+      --input $EMB_FILE \
+      --output ${OUTPUT_FILE} \
+      --meaning_only;
   done;
 done;
 
-echo -e '\e[41m\e[1m 2b. patch, merge and sort everything. \e[0m';
+echo -e '\e[33m\e[1m 2b. patch, merge and sort everything. \e[0m';
 ## everything must be sorted
 for EMB_ARCH in USE DAN infersent skipthoughts randlstm randtf; do
-  echo -e "\e[41m\e[1m sort ${EMB_ARCH} output JSONs \e[0m";
-  python3 src/shared/merge.py --sort $(find data/embs_exp3/${EMB_ARCH} -type f -name '*.json') --key_meanings ${EMB_ARCH};
+  echo -e "\e[33m\e[1m sort ${EMB_ARCH} output JSONs \e[0m";
+  python3 src/shared/merge.py \
+    --sort $(find data/embs_exp3/${EMB_ARCH} -type f -name '*.json') \
+    --key_meanings ${EMB_ARCH};
 done;
 ## merge results per run
 for RUN in $(seq 1 5); do
-  echo -e "\e[41m\e[1m merge all results for run ${RUN} \e[0m";
-  python3 src/shared/merge.py --merge $(find data/embs_exp3/ -type f -name "run-${RUN}.json" ) --merged_file data/results_exp3/dists/${RUN}.json;
+  echo -e "\e[33m\e[1m merge all distances for run ${RUN} \e[0m";
+  python3 src/shared/merge.py \
+    --merge $(find data/embs_exp3/ -type f -name "run-${RUN}.json" ) \
+    --merged_file data/results_exp3/dists/${RUN}.json;
 done;
 
-echo -e '\e[41m\e[1m 3. compute Mantel tests \e[0m';
-python3 src/shared/compute_mantels_per_dir.py --input_dir data/results_exp3/dists/ --output data/results_exp3/mantels/results.txt;
+echo -e '\e[33m\e[1m 3. compute Mantel tests \e[0m';
+python3 src/shared/compute_mantels_per_dir.py \
+  --input_dir data/results_exp3/dists/ \
+  --output data/results_exp3/mantels/results.txt;
 
 deactivate;
